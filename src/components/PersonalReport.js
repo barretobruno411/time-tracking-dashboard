8d59e4e2afc32465ffb3ac3bdb1a0cfe5cf482c4
Bruno Duarte <bruno.duarte411@gmail.com>
2022-07-25 16:39:33 -0300

    mobile style complete

--- src/components/PersonalReport.js
+++ src/components/PersonalReport.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import image from '../images/image-jeremy.png'
+import React from "react";
+import "./PersonalReport.css";
+import image from "../images/image-jeremy.png";
 
 const PersonalReport = () => {
   return (
-    <div className='personalReport'>
-        <div className='main'>
-            <img src={image} alt="" />
-            <h2>Report for</h2>
-            <h1>Jeremy Robson</h1>
+    <div className="personalReport">
+      <div className="main">
+        <img src={image} alt="person" />
+        <div className="textContent">
+          <h2>Report for</h2>
+          <h1>Jeremy Robson</h1>
         </div>
-        <div className="timeFrames">
-            <ul className='timeFramesList'>
-                <li>Daily</li>
-                <li>Weekly</li>
-                <li>Montly</li>
-            </ul>
-        </div>
+      </div>
+      <div className="timeFrames">
+        <ul className="timeFramesList">
+          <li>Daily</li>
+          <li>Weekly</li>
+          <li>Montly</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PersonalReport
+export default PersonalReport;
